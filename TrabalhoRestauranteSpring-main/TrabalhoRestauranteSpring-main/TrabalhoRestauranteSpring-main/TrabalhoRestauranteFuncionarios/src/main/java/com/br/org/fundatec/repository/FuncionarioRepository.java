530c73fe53f94b145ee5150cc60211bc2c1177ea
MatheusRanheri <matheusranheri@gmail.com>
2025-04-01 16:31:36 -0300

    Delete TrabalhoRestauranteSpring-main/TrabalhoRestauranteSpring-main/TrabalhoRestauranteSpring-main/TrabalhoRestauranteFuncionarios directory

--- TrabalhoRestauranteSpring-main/TrabalhoRestauranteSpring-main/TrabalhoRestauranteSpring-main/TrabalhoRestauranteFuncionarios/src/main/java/com/br/org/fundatec/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.org.fundatec.repository;
-
-import com.br.org.fundatec.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
