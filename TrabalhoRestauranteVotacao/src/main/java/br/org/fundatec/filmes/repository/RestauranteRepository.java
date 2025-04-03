dfdff47cc193494eca7d4ceff3dc97f486c58b23
MatheusRanheri <matheusranheri@gmail.com>
2025-04-03 19:24:44 -0300

    Delete TrabalhoRestauranteVotacao directory

--- TrabalhoRestauranteVotacao/src/main/java/br/org/fundatec/filmes/repository/RestauranteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.org.fundatec.filmes.repository;
-
-import br.org.fundatec.filmes.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
-}
